dfcbd551414949b7d8cda39c9ad90283a13250f3
CanseFr <jucansell@gmail.com>
2024-03-13 19:39:59 +0100

    :rotating_light: Run lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,52 +1,50 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import * as Joi from "joi";
-import { DatabaseModule } from "./common/database/database.module";
-import { GraphQLModule } from "@nestjs/graphql";
-import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
-import { UsersModule } from "./users/users.module";
-import { LoggerModule } from "nestjs-pino";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { DatabaseModule } from './common/database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UsersModule } from './users/users.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
-        MONGODB_URI: Joi.string().required()
-      })
+        MONGODB_URI: Joi.string().required(),
+      }),
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true
+      autoSchemaFile: true,
     }),
     DatabaseModule,
     UsersModule,
     LoggerModule.forRootAsync({
       useFactory: (configService: ConfigService) => {
-
-        const isProduction = configService.get("NODE_ENV") === "production;";
+        const isProduction = configService.get('NODE_ENV') === 'production;';
 
         return {
           pinoHttp: {
             transport: isProduction
               ? undefined
               : {
-                target: "pino-pretty",
-                options: {
-                  singleLine: true
-                }
-              },
-            level: isProduction ? "info" : "debug"
-          }
+                  target: 'pino-pretty',
+                  options: {
+                    singleLine: true,
+                  },
+                },
+            level: isProduction ? 'info' : 'debug',
+          },
         };
       },
-      inject: [ConfigService]
-    })
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
